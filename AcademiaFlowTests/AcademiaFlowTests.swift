71aaa0c3ca037460c7e590a9a25f93188da16f8f
@shubham_blusmart <shubham.tomar@blu-smart.com>
2025-03-31 13:11:34 +0530

    Initial Commit

---
+++ AcademiaFlowTests/AcademiaFlowTests.swift
@@ -0,0 +1,16 @@
+//
+//  AcademiaFlowTests.swift
+//  AcademiaFlowTests
+//
+//  Created by Shubham Tomar on 31/03/25.
+//
+
+import Testing
+
+struct AcademiaFlowTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
